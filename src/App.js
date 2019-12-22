9cacea572c39bcb19d928a4d3c210bf2cb9b0975
minisola <blackkid1989@gmail.com>
2019-12-22 20:12:30 +0800

    @根路径,加入loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Welcome from './pages/Welcome'
 import Form from './pages/Form'
 import './css/app.scss';
 
 function App() {
+  const [display, setDisplay] = useState('none')
+  useEffect(() => {
+    // Update the document title using the browser API
+    const paths = [
+      require('@/img/index-feature.png'),
+      require('@/img/form-bg.png'),
+      require('@/img/form-reminder.png'),
+      require('@/img/index-fosun-logo.png'),
+      require('@/img/index-goods.png'),
+      require('@/img/index-goods-circle.png')
+    ]
+    const imgHandleQuery = paths.map(el=>(imgOnload(el)))
+  
+    Promise.all(imgHandleQuery).then(()=>{
+      const $pageLoading = document.querySelector('.page-loading')
+      $pageLoading.parentNode.removeChild($pageLoading)
+      setDisplay(true)
+    })
+
+    console.log('effect');
+  },[]);
+
   return (
     <div className="app-container">
-      <div className="App">
+      <div className="App" style={{display: display}}>
         <Router>
           <Switch>
             <Route exact path='/' component={Welcome} />
@@ -16,8 +38,27 @@ function App() {
         </Router>
       </div>
     </div>
-
   );
+}
+  
+function imgOnload(path) {
+  return new Promise((resolve,reject)=>{
+      let time = 0
+      const img = new Image()
+      img.src = path
+      //计时器->30秒仍然无法载入则直接通过
+      const timeInterval = setInterval(()=>{
+        time++
+        if(time>=30000) {
+          clearInterval(timeInterval) 
+          resolve()
+        }
+      })
+      img.onload = ()=>{
+        clearInterval(timeInterval) 
+        resolve()
+      }
+  })
 }
 
 export default App;
